f0c3d623218450d6c3699d50532a8256229d8436
ollivv3r <oliverwahl24@gmail.com>
2023-05-24 22:49:05 -0400

    quilt pattern done
    created a quilt pattern that was inspired by an actual quilt square

--- week2/2.4.quilt-pattern/sketch.js
+++ week2/2.4.quilt-pattern/sketch.js
@@ -1,7 +1,57 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1200, 1200);
+  noLoop();
 }
 
 function draw() {
   background(220);
+
+  translate(100,100);
+  rect(0,0,1000,1000);
+
+  for (let x = 0; x < 10; x++){
+    for (let y = 0; y < 10; y++){
+
+      // this is the checkerboard conditional
+       if((y + x) % 2 == 0){ 
+
+      // this is the quadrant conditional
+      // if((x < 5 & y < 5) | (x >= 5 & y >= 5)) {
+
+       push();
+     translate(x * 100, y * 100);
+     fill("white")
+     rect(0,0,100,100);
+    
+ 
+      fill("lightblue")
+     triangle(100,0,0,100,100,100);
+     fill("pink")
+     triangle(50,50,50,100,100,100);
+     fill("lightgreen")
+     triangle(50,50,100,50,100,100)
+      fill("plum")
+     triangle(50,50,100,50,100,0);
+     pop();
+
+      }else{
+        push();
+     translate(x * 100, y *100);
+     fill("white")
+
+     rect(0,0,100,100);
+       fill("lightblue")
+     triangle(100,0,0,100,100,100);
+     fill("pink")
+     triangle(50,50,50,100,100,100);
+     fill("lightgreen")
+     triangle(50,50,100,50,100,100);
+     fill("plum")
+     triangle(50,50,100,50,100,0);
+      pop();
+      }
+
+
+    }
+  }
 }
