a66dfc541c65206d95e09c7365ad4ca18fe1d41e
ollivv3r <oliverwahl24@gmail.com>
2023-06-04 15:22:02 -0400

    time

--- week3/time/sketch.js
+++ week3/time/sketch.js
@@ -1,7 +1,44 @@
+let x_hour = 50
+let x_minute = 50
+let x_second = 50
+
+var r = 0
+var b = 255
+var p = 550
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(600, 600);
+  angleMode(DEGREES);	// change the angle mode from radians to degrees
 }
 
 function draw() {
-  background(220);
+  background("gray");
+  stroke("white");
+ // text("hour " + hour(), 50, 50);
+  //text("minute " + minute(), 50, 75);
+  //text("second " + second(), 50, 100);
+
+  push();
+  noStroke();
+
+  x_hour = map(hour(), 0, 23, 50, 550);
+  r = map(x_hour, 50, 550, 0, 255);
+  b = map(x_hour, 50, 550, 255, 0);
+  fill(r, 100, b);
+  circle(x_hour, 200, 80);
+
+  x_minute = map(minute(), 0, 59, 50, 550);
+  r = map(x_minute, 50, 550, 0, 255);
+  b = map(x_hour, 50, 550, 255, 0);
+  fill(r, 100, b);
+  circle(x_minute, 350, 50);
+
+  x_second = map(second(), 0, 59, 50, 550);
+  r = map(x_second, 50, 550, 0, 255);
+  b = map(x_hour, 50, 550, 255, 0);
+  fill(r, 100, b);
+  circle(x_second, 500, 30);
+
+  pop();
+
 }
