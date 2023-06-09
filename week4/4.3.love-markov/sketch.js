37ebd0a23ada919bf75e32a8f634137af2c2aa67
ollivv3r <oliverwahl24@gmail.com>
2023-06-09 14:24:19 -0400

    love-markov assignment

--- week4/4.3.love-markov/sketch.js
+++ week4/4.3.love-markov/sketch.js
@@ -1,7 +1,38 @@
+let rm = RiTa.markov(4);
+
+let data = document.getElementById("source").innerText;
+
+rm.addText(data);
+
+
+
+
+
+
 function setup() {
-  createCanvas(800, 800);
+
+// generate 6 sentences
+  let lines = rm.generate(6);
+
+// find the letter DIV
+  let letter = select("#letter");
+
+//add greeting
+letter.child( createP("Dear Niko,"))
+
+for( let l = 0; l < lines.length; l++){
+  letter.child(
+      createP(lines[l]));
 }
 
-function draw() {
-  background(220);
+// add closing
+letter.child ( createP("Your Love, Oliver"))
+}
+
+
+function mousePressed(){
+  letter.remove();
+  letter = createDiv();
+  letter.id("letter");
+  setup();
 }
